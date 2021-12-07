c0a491842b669143a472cb686402a4c4f83d56d3
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2021-12-07 13:41:37 -0600

    Added background image , added photo options

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
 const nameInput = document.getElementById('name-input');
 //get the span for the band name
 const bandName = document.getElementById('band-name');
+const nameDisplay = document.getElementById('name-display');
 //add event listener for input changing
-nameInput.addEventListener('input', () => {
-    console.log('input changed', nameInput.value);
+//nameInput.addEventListener('input', () => {
+  //  nameDisplay.textContent = nameInput.value;
     //when it changes, update the value for span
-});
+//});
+
 
