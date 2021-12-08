c23b2369778fd91c529b2d060c2cd4c6d6069b3c
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2021-12-07 18:12:09 -0600

    I added JavaScript features and got my input and display boxes to connect.

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
+//$('textarea').autoResize();
 
-$('textarea').autoResize();
-/*
 //Get the input name, identify the value, display the current value of the input
 const nameInput = document.getElementById('name-input');
 const nameDisplay = document.getElementById('name-display');
+console.log(nameInput, nameDisplay);
+const descriptionDisplay = document.getElementById('description-display');
+const descriptionInput = document.getElementById('description-input');
+console.log(descriptionDisplay, descriptionInput);
+const imageChoice = document.getElementById('image-choice');
+const image = document.getElementById('image');
+console.log(imageChoice, image);
+
+
 nameInput.addEventListener('input', () => {
     nameDisplay.textContent = nameInput.value;
 });
 
-
-// Description Box adjustments
-const descriptionTextarea = document.getElementById('description-textarea');
-const descriptionDisplay = document.getElementById('description-display');
-descriptionTextarea.addEventListener('input', () => {
-    descriptionDisplay.textContent = descriptionTextarea.value;
+descriptionInput.addEventListener('input', () => {
+    descriptionDisplay.textContent = descriptionInput.value;
 });
 
-// Theme
-const themeSelect = document.getElementById('theme-select');
-const character = document.getElementById('character');
-themeSelect.addEventListener('change', () => {
-    character.classList.value = '';
-    character.classList.add(themeSelect.value);
+imageChoice.addEventListener('change', () => {
+    image.src = imageChoice.value;
 });
-*/
